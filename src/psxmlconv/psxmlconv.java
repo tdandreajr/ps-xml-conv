36dba951aa9f12db2f52bbdc9f70059e81456782
tdandrea <tdandrea@WIN-TDANDREA.lexucg.local>
2013-09-25 15:26:46 -0400

    Application now reads all files and creates a directory listing.

--- src/psxmlconv/psxmlconv.java
+++ src/psxmlconv/psxmlconv.java
@@ -16,42 +16,39 @@ public class psxmlconv {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    String path = "."; 
+    String path = System.getProperty("user.dir"); 
     ArrayList<String> ps_filelist = new ArrayList<>();
 
     if (project_exists(path)!= true) {
       System.out.println("Project XML Definition could not be found in the project root directory.");
       System.exit(1);
     }
-    
+    System.out.println("Project definition exists, begin file loop.");
     File project_path = new File(path);
-    ArrayList<File> ps_dirlist;
-    ps_dirlist = new ArrayList<File>(Arrays.asList(project_path.listFiles()));
-    for (File psd : ps_dirlist) {
-      if (psd.isDirectory()) {
-        System.out.println(psd.toString());
+    ArrayList<File> files = new ArrayList<File>(Arrays.asList(project_path.listFiles()));
+    ArrayList<File> directories = new ArrayList<File>();
+    int counter = 0;
+    for (File f : files) {
+      if (f.isDirectory()) {
+        directories.add(f);
+        counter++;
+      }
     }
+    
+    int dcounter = 0;
+    for (File f : directories) {
+      System.out.println(f.getAbsolutePath());
+      dcounter++;
+    }
+    
+    System.out.println("------------------------------");
+    System.out.println("File counter:" + String.valueOf(counter));
+    System.out.println("Dir counter :" + String.valueOf(dcounter));
     System.exit(0);
-    }
   }
   
   public static boolean project_exists(String p) {
-    File project_file = new File(p + "\\project.xml");
+    File project_file = new File(p + "/project.xml");
     return project_file.exists();
-  }
-  
-  public static void temp_method() {
-    String path = "";
-    String files = "";
-    File folder = new File(path);
-    File[] listOfFiles = folder.listFiles(); 
-    for (int i = 0; i < listOfFiles.length; i++) {
-      if (listOfFiles[i].isFile()) {
-        files = listOfFiles[i].getName();
-        if (files.endsWith(".sed") || files.endsWith(".SED")) {
-          System.out.println(files);
-        }
-      }
-    }
   }
 }
