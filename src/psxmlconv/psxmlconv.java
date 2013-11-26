d56db33a94ba5e84d307211a940aaa3aff591a50
tdandrea <tdandrea@WIN-TDANDREA.lexucg.local>
2013-11-26 16:19:43 -0500

    Updates project name to LXCOMPARE to standardize naming for diffs.

--- src/psxmlconv/psxmlconv.java
+++ src/psxmlconv/psxmlconv.java
@@ -13,7 +13,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Map;
 import java.util.TreeMap;
-import java.util.LinkedList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.xml.stream.XMLInputFactory;
@@ -134,7 +133,11 @@ public class psxmlconv {
                 if(fileNumber.equals(1) && nodeDepth.equals(1)){
                   writer.writeStartElement(currElement);
                   for (Integer attr=0; attr<reader.getAttributeCount(); attr++) {
-                    writer.writeAttribute(reader.getAttributeLocalName(attr), reader.getAttributeValue(attr));
+                    String attrVal = reader.getAttributeValue(attr);
+                    if (currElement.equals("project")){
+                      attrVal = "LXCOMPARE";
+                    }
+                    writer.writeAttribute(reader.getAttributeLocalName(attr), "LXCOMPARE");
                   }
                 }
                 if(fileNumber.equals(1) && nodeDepth.equals(2)){
